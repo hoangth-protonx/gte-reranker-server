0af65a88dfc3cded5158e6618f3ec6f7738f62c4
hoangth-protonx <hoangth@protonx.co>
2026-03-25 09:59:20 +0700

    change port for server

--- server.py
+++ server.py
@@ -203,4 +203,4 @@ async def rerank(request: RerankRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=12345)
